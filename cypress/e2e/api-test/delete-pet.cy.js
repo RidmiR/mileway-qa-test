2aaca06770d3e65f04463e3162e6749de0b7d8f5
Lokumudalige, Ridmi Rangika <ridmi.rangika@miele.com>
2023-10-14 00:23:02 +0200

    edit delete pet store test case

--- cypress/e2e/api-test/delete-pet.cy.js
+++ cypress/e2e/api-test/delete-pet.cy.js
@@ -3,28 +3,29 @@ describe('DELETE /pet/{petId} API', () => {
     const petStoreData = "api_pet_store_order.json";
     const baseUrl = "https://petstore.swagger.io/v2";
 
-    it('delete existing pet record', () => {
+    // it('delete existing pet record', () => {
 
-        cy.fixture(petStoreData).as("orderData").then(($body) => {
+    //     cy.fixture(petStoreData).as("orderData").then(($body) => {
 
-            cy.request({
-                "method": "POST",
-                "url": baseUrl + '/store/order',
-                "body": $body
-            })
-                .then((response) => {
-                    expect(response.status).to.eq(200);
-                    // Send a DELETE request to the /store/order/{orderId} endpoint
-                    cy.request({
-                        "method": "DELETE",
-                        "url": baseUrl + '/store/order/' + response.body.id,
-                    }).then((response) => {
-                        // Assert the response status code
-                        expect(response.status).to.eq(200);
-                    });
-                })
-        })
-    })
+    //         cy.request({
+    //             "method": "POST",
+    //             "url": baseUrl + '/store/order',
+    //             "body": $body
+    //         })
+    //             .then((response) => {
+    //                 expect(response.status).to.eq(200);
+    //                 cy.log(response.body.id)
+    //                 // Send a DELETE request to the /store/order/{orderId} endpoint
+    //                 cy.request({
+    //                     "method": "DELETE",
+    //                     "url": baseUrl + '/store/order/' + response.body.id,
+    //                 }).then((response) => {
+    //                     // Assert the response status code
+    //                     expect(response.status).to.eq(200);
+    //                 });
+    //             })
+    //     })
+    // })
 
     it('delete invalid oder id and verify the status', () => {
 
